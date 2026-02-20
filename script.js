296d445f874ac637a066d94f5c17a1ce60b25ea5
Rajputharsh25 <144730809+Rajputharsh25@users.noreply.github.com>
2026-02-20 19:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-function toRoman(num) {
+function convertToRoman(num) {
+  const romanSymbols = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
+  ];
 
-    const symbols = [
-        ['M', 1000],
-        ['CM', 900],
-        ['D', 500],
-        ['CD', 400],
-        ['C', 100],
-        ['XC', 90],
-        ['L', 50],
-        ['XL', 40],
-        ['X', 10],
-        ['IX', 9],
-        ['V', 5],
-        ['IV', 4],
-        ['I', 1]
-    ];
+  let result = '';
 
-    let result = "";
-
-    for (let i = 0; i < symbols.length; i++) {
-        while (num >= symbols[i][1]) {
-            result += symbols[i][0];
-            num -= symbols[i][1];
-        }
+  // Iterate through the symbols from largest to smallest
+  for (const [symbol, value] of romanSymbols) {
+    // While the current value fits into the remaining number
+    while (num >= value) {
+      result += symbol; // Add the symbol to our string
+      num -= value;     // Subtract the value from our total
     }
+  }
 
-    return result;
+  return result;
 }
 
-
-// Examples
-console.log(toRoman(14));  // XIV
-console.log(toRoman(798)); // DCCXCVIII
+// Examples:
+console.log(convertToRoman(14));      // Output: XIV
+console.log(convertToRoman(798));     // Output: DCCXCVIII
+console.log(convertToRoman(3999));    // Output: MMMCMXCIX
